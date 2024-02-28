a148a116ee749aa30af321ac4ebe159bca1a46bf
Tyler Gebel <tgebel33@gmail.com>
2024-02-28 10:44:55 -0800

    Attempted to calculate keys in keygen file. Switch statement may not be necessary as a conditional statement may work just fine.

--- keygen.c
+++ keygen.c
@@ -14,6 +14,17 @@ Date: 2-28-24
 
 
 int main(int argc, char* argv[]) {
+  character = 'A'; // This is used as starting ASCII Value
+  int count = 0;
 
+  if (argc == 2) {
+    int length = atoi(argv[1]);
+    while (count < length) {
+     int key = rand() % 27;
+      swtich (key) {
+        case 26:
+          printf(" ");
+        case
+    
 
 }
