adbbf9848b4154c5f6068751d15074df42e36932
Tyler Gebel <tgebel33@gmail.com>
2024-03-11 17:26:27 -0700

    Added error logic for enc_server.c

--- enc_client.c
+++ enc_client.c
@@ -145,7 +145,8 @@ int main(int argc, char *argv[]) {
       charsRead = recv(socketFD, buffer, sizeof(buffer) - 1, 0);
     }
     if (strcmp(buffer, "confirmSize") != 0) {
-      fprintf(stderr, "Buffer: %s,There was an error confirming the size with the server", buffer);
+      buffer[sizeof(buffer)- 1] = 0;
+      fprintf(stderr, "Buffer: %s There was an error confirming the size with the server\n", buffer);
     }
     memset(buffer, '\0', sizeof(buffer));
 
